f8ff0302ae8e0af0dfedcdce5da99ad78fb51594
Vishesh-panghal <visheshpanghal12@gmail.com>
2023-08-12 13:29:43 +0530

    create database and modal of Student list

---
+++ lib/provider_practice/list_database.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: constant_identifier_names
+
+import 'dart:io';
+
+import 'package:authentication_pages/provider_practice/student_model.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class ListDatabase {
+  ListDatabase._();
+  static final ListDatabase db = ListDatabase._();
+
+  Database? _database;
+
+  static const NOTE_TABLE = 'note';
+  static const NOTE_COLUMN_ID = 'column_id';
+  static const NOTE_COLUMN_TITLE = 'column_title';
+  static const NOTE_COLUMN_CLASS = 'column_class';
+
+  Future<Database> getDB() async {
+    if (_database != null) {
+      return _database!;
+    } else {
+      return await initDB();
+    }
+  }
+
+  Future<Database> initDB() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    var dbPath = join(documentDirectory.path, 'StudentList.db');
+    return openDatabase(
+      dbPath,
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+            'Create table $NOTE_TABLE ($NOTE_COLUMN_ID integer primary key autoincement),$NOTE_COLUMN_TITLE text,$NOTE_COLUMN_CLASS text');
+      },
+    );
+  }
+
+  Future<bool> addList(studentModal stdnt) async {
+    var db = await getDB();
+    int rowsEffected = await db.insert(NOTE_TABLE, {
+      ListDatabase.NOTE_COLUMN_TITLE: stdnt.title,
+      ListDatabase.NOTE_COLUMN_CLASS: stdnt.cls,
+    });
+    if (rowsEffected > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> updateList(studentModal stdnt) async {
+    var db = await getDB();
+    var count = await db.update(NOTE_TABLE, stdnt.toMap(),
+        where: '$NOTE_COLUMN_ID = ? ', whereArgs: ['${stdnt.id}']);
+    return count > 0;
+  }
+
+  Future<bool> deleteList(int id) async {
+    var db = await getDB();
+    var count = await db
+        .delete(NOTE_TABLE, where: "$NOTE_COLUMN_ID=?", whereArgs: [id]);
+    return count > 0;
+  }
+
+  Future<List<studentModal>>fetchAllList()async{
+    var db = await getDB();
+    List<Map<String,dynamic>> lists = await db.query(NOTE_TABLE);
+    List<studentModal> studentList = [];
+    for(Map<String,dynamic> list in lists)
+    {
+      studentList.add(studentModal.fromMap(list));
+    }
+    return studentList;
+  }
+}
---
+++ lib/provider_practice/student_model.dart
@@ -0,0 +1,27 @@
+import 'list_database.dart';
+
+class studentModal
+{
+  int? id;
+  String title;
+  String cls;
+
+  studentModal({this.id,required this.title,required this.cls});
+
+  factory studentModal.fromMap(Map<String,dynamic> map)
+  {
+    return studentModal(
+      id: map[ListDatabase.NOTE_COLUMN_ID],
+      title: map[ListDatabase.NOTE_COLUMN_TITLE], cls: map[ListDatabase.NOTE_COLUMN_CLASS]);
+  }
+
+  Map<String,dynamic> toMap()
+  {
+    return 
+    {
+      ListDatabase.NOTE_COLUMN_ID:id,
+      ListDatabase.NOTE_COLUMN_TITLE:title,
+      ListDatabase.NOTE_COLUMN_CLASS:cls,
+    };
+  }
+}
