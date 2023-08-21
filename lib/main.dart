fa050db4b240abd218311508e62db21ff3adb981
Vishesh-panghal <visheshpanghal12@gmail.com>
2023-08-21 15:15:58 +0530

    Internet connectivity check modal on BLoc

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'BLoc_Exercide/InternetConnectivity/screens/netConn.dart';
 
-
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp(
-      home: ,
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: IsConnectPage(),
     );
-    
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_macos
 import flutter_local_notifications
 import path_provider_foundation
 import rive_common
@@ -12,6 +13,7 @@ import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
